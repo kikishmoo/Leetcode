0c5309c7ae7d9a5b8b01084551657760d3f8fcb5
Kiki Greene <kigreene@seas.upenn.edu>
2023-12-12 13:16:48 -0500

    Create 485_MaxConsecutiveOnes.c

---
+++ 485_MaxConsecutiveOnes.c
@@ -0,0 +1,21 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int* start = nums;
+    int count = 0;
+    int max_count = 0;
+    for (int i = 0; i < numsSize; i++){
+        if(*nums == 1){
+            count++;
+        }
+        if(*nums == 0){
+            if (count > max_count){
+                max_count = count;
+            }
+            count = 0;
+        }
+        nums++;
+    }
+    if (count > max_count){
+        max_count = count;
+    }
+    return max_count;
+}
