81c1f1e6c5e61cb95a24b10241f2c8909f09d1c1
Kiki Greene <kigreene@seas.upenn.edu>
2023-12-12 13:36:09 -0500

    Create 1295_FindNumberswithEvenNumberOfDigits.c

---
+++ 1295_FindNumberswithEvenNumberOfDigits.c
@@ -0,0 +1,19 @@
+// 1295. Find Numbers with Even Number of Digits
+
+int findNumbers(int* nums, int numsSize) {
+    int count = 0;
+    int num;
+    for (int i = 0; i < numsSize; i++){
+        int digit = 1;
+        num = *nums / 10;
+        while (num){
+            num = num / 10;
+            digit++;
+        }
+        if (digit % 2 == 0){
+            count++;
+        }
+        nums++;
+    }
+    return count;
+}
