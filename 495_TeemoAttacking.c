4051052cae07b441ffe2dd56966d58a30fceb9f2
Kiki Greene <kigreene@seas.upenn.edu>
2023-12-12 16:58:49 -0500

    Update 495_TeemoAttacking.c

--- 495_TeemoAttacking.c
+++ 495_TeemoAttacking.c
@@ -1,2 +1,21 @@
 // 495. Teemo Attacking
 
+int findPoisonedDuration(int* timeSeries, int timeSeriesSize, int duration) {
+    if (duration == 0) {
+        return 0;
+    }
+    int attack = 0;
+    int second = *timeSeries + duration -1;
+    attack += duration;
+    int i = 0; 
+    while (i < timeSeriesSize-1){
+        while (*timeSeries < second){
+            timeSeries++;
+            i++;
+        }
+        second = *timeSeries + duration -1;
+        attack += duration;
+        i++;
+    }
+    return attack;
+}
