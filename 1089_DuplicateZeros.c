316a6e5a6611d40216b15c3666b41a0687223bd3
Kiki Greene <kigreene@seas.upenn.edu>
2023-12-18 17:02:29 -0500

    Update 1089_DuplicateZeros.c

--- 1089_DuplicateZeros.c
+++ 1089_DuplicateZeros.c
@@ -6,30 +6,41 @@ void duplicateZeros(int* arr, int arrSize) {
     int* next = arr + 1;
     int temp_a = -1;
     int temp_n = -1;
-    while (arr < end-1){
+    while (arr < end){
         if (*arr == 0){
             temp_n = *next;
             *next = 0;
-            arr += 2;
+            if (temp_a != -1){
+                *arr = temp_a;
+                temp_a = *next;
+                
+                
+            }
+            arr ++;
+            if (arr == end){
+                *arr = temp_n;
+                break;
+            }
+            next ++;
+            temp_a = *next;
+            *next = temp_n;
+            arr++;
+            next++;
+        } else if (*next == 0){
+            arr++;
+            next++;
+        } else {
+            arr++;
             if (arr == end){
                 *arr = temp_a;
                 break;
             }
-            next += 2;
-            if (temp_n != -1){
-                temp_a = *arr;
-                *arr = temp_n;
-                temp_n = *next;
-            }
-        } else {
-            arr++;
             next++;
             if (temp_n != -1){
+                temp_n = *arr;
                 *arr = temp_a;
                 temp_a = *next;
                 *next = temp_n;
-                // arr++;
-                // next++;
             }
             
         }
