f343ec8b79f95624d61cd90d9b7cd15bfbe0e1c3
Kiki Greene <kigreene@seas.upenn.edu>
2023-12-18 23:53:25 -0500

    Update 1089_DuplicateZeros.c

--- 1089_DuplicateZeros.c
+++ 1089_DuplicateZeros.c
@@ -1,6 +1,7 @@
 // 1089. Duplicate Zeros
-// 17 December 2023
+// 18 December 2023
 
+// v1
 void duplicateZeros(int* arr, int arrSize) {
     int* end = arr + arrSize -1;
     int* next = arr + 1;
@@ -41,6 +42,82 @@ void duplicateZeros(int* arr, int arrSize) {
                 *arr = temp_a;
                 temp_a = *next;
                 *next = temp_n;
+            }
+            
+        }
+        
+    }
+}
+
+// v2
+void duplicateZeros(int* arr, int arrSize) {
+    int* end = arr + arrSize -1;
+    int* next = arr + 1;
+    int temp_a = -1;
+    int temp_n = -1;
+    while (arr < end){
+        if (*arr == 0){
+            if (temp_n != -1){
+                *arr = temp_n;
+            }
+            temp_n = *next;
+            *next = 0;
+            arr++;
+            if (arr == end && temp_a != -1){
+                *arr = temp_a;
+                break;
+            }
+            next++;
+            
+            temp_a =temp_n;
+            temp_n = *next;
+            if  (*arr == 0 && *(arr-1) != 0){
+                *next = 0;
+                arr++;
+                if (arr == end && temp_a != -1){
+                    *arr = temp_a;
+                    break;
+                }
+                next++;
+            }
+            *next = temp_a;
+            arr++;
+            if (arr == end && temp_a != -1){
+                *arr = temp_a;
+                break;
+            }
+            next++;
+            
+            
+            
+            
+        } else {
+            if (temp_n != -1){
+                temp_a = temp_n;
+                temp_n = *next;
+                if  (*arr == 0 && *(arr-1) != 0){
+                    *next = 0;
+                    arr++;
+                    if (arr == end && temp_a != -1){
+                        *arr = temp_a;
+                        break;
+                    }
+                    next++;
+                }
+                *next = temp_a;
+                arr++;
+                if (arr == end && temp_a != -1){
+                    *arr = temp_a;
+                    break;
+                }
+                next++;
+            } else {
+                arr++;
+                if (arr == end && temp_a != -1){
+                    *arr = temp_a;
+                    break;
+                }
+                next++;
             }
             
         }
